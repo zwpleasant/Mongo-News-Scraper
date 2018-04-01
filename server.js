47520932fcd8fc5b4ad7a516e1bcae7d726d48b4
Zachary Pleasant <zpleasant10@gmail.com>
2018-04-01 10:42:22 -0500

    Add routes to grab articles in general and by ID

--- server.js
+++ server.js
@@ -24,37 +24,67 @@ mongoose.connect("mongodb://localhost/mongoHeadlines");
 
 // a GET route for scraping NPR News headlines
 app.get("/scrape", function(req, res) {
-    // First, we grab the body of the html with request
-    axios.get("https://www.npr.org/sections/news/").then(function(response) {
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(response.data);
-  
-      // Now, we grab every h2 within an title tag, and do the following:
-      $("h2.title").each(function(i, element) {
-        // Save an empty result object
-        var result = {};
-  
-        // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this).text();
-        result.link = $(this).children().attr("href");
-        result.summary = $(this).next('.teaser').children().text();
-  
-        // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-          .then(function(dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function(err) {
-            // If an error occurred, send it to the client
-            return res.json(err);
-          });
-      });
-  
-      // If we were able to successfully scrape and save an Article, send a message to the client
-      res.send("Scrape Complete");
+  // First, we grab the body of the html with request
+  axios.get("https://www.npr.org/sections/news/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an title tag, and do the following:
+    $("h2.title").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).text();
+      result.link = $(this).children().attr("href");
+      result.summary = $(this).next('.teaser').children().text();
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
     });
+
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.send("Scrape Complete");
   });
+});
+
+// GET Route for getting all Articles from the db
+app.get("/articles", function (req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function (dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// GET Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function (req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function (dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
 // start the server
 app.listen(PORT, function() {
